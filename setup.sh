4320ecda0a163b11da6b4bda6288e130fd582528
operezcham <operezcham@gmail.com>
2021-11-09 14:55:30 -0600

    🌐

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ apt -y install git
 apt -y install nodejs-legacy
 apt -y install postgresql
 git clone https://github.com/operezcham90/vault-server.git
-node vault-server/server.js
+node vault-server/app/server.js
