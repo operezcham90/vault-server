4d61f39bf3c929f6643680d1a2971c709ed895dc
operezcham <operezcham@gmail.com>
2021-11-10 17:45:30 -0600

    🌐

--- app/cert.js
+++ app/cert.js
@@ -1,10 +1,44 @@
+'use strict'
+
 const fs = require('fs')
+const { exec } = require("child_process")
+const { networkInterfaces } = require('os');
 
-const content = 'Some content!'
-
-fs.writeFile('./req', content, err => {
-    if (err) {
-        console.error(err)
-        return
+const req = {
+    ip: '192.168.0.1',
+    text: '',
+    erase: () => {
+        req.text = ''
+    },
+    section: (name) => {
+        req.text += '[' + name + ']\n'
+    },
+    property: (name, value) => {
+        req.text += name + ' = ' + value
+    },
+    build: () => {
+        req.erase()
+        req.section('req')
+        req.property('default_bit', 4096)
+        req.property('default_md', 'sha256')
+        req.property('distinguished_name', 'req_distinguished_name')
+        req.property('x509_extensions', 'v3_req')
+        req.property('prompt', 'no')
+        req.section('req_distinguished_name')
+        req.property('O', 'Home')
+        req.property('OU', 'Storage')
+        req.property('CN', req.ip)
+        req.section('v3_req')
+        req.property('keyUsage', 'keyEncipherment, dataEncipherment')
+        req.property('extendedKeyUsage', 'serverAuth')
+        req.property('subjectAltName', '@alt_names')
+        req.section('alt_names')
+        req.property('IP.1', req.ip)
+    },
+    write: async () => {
+        const err = await fs.writeFile('./req', req.text)
+        if (err) {
+            console.error(err)
+        }
     }
-})
+}
