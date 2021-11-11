9a85d11e2d72783670332a42b0a01b760c43ef39
operezcham <operezcham@gmail.com>
2021-11-11 09:19:03 -0600

    🌐

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const cert = require('./cert.js')
 
 const start = async () => {
     await cert.run()
-    https.createServer({}, (req, res) => {
+    https.createServer(cert.value, (req, res) => {
         res.writeHead(200)
         res.end('hello world\n')
         console.log('hello world\n')
