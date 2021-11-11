5cb53ae333d6111f290b598ae2ca402eab392c6b
operezcham <operezcham@gmail.com>
2021-11-11 01:14:23 -0600

    🌐

--- app/cert.js
+++ app/cert.js
@@ -69,7 +69,7 @@ const cert = {
         await req.write()
         const res = await util.promisify(cp.execFile)(cert.command, cert.arguments)
         console.log(res)
-        cert.value.cert = await fs.promises.readFile('./domain.cert')
+        cert.value.cert = await fs.promises.readFile('./domain.crt')
         cert.value.key = await fs.promises.readFile('./domain.key')
     }
 }
