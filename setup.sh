870cc85556114cf1356de4002fc8de2333a2ded8
operezcham <operezcham@gmail.com>
2021-11-28 02:48:15 -0600

    💿

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 mkdir /home/server
 cd /home/server
-printf "APP_KEY=" >> .env
-uuidgen -r >> .env
-printf "HOST=" >> .env
-hostname -I >> .env
-echo "* Environment ready."
+echo "* Folder ready."
 
 apt -y update
 echo "* Packages updated."
 
 apt -y install git
 git clone https://github.com/operezcham90/vault-server.git .
+printf "APP_KEY=" >> .env
+uuidgen -r >> .env
+printf "HOST=" >> .env
+hostname -I >> .env
 cat resources/env >> .env
 echo "* Git installed."
 
@@ -21,7 +21,7 @@ echo "* PostgreSQL installed."
 
 apt -y install npm
 npm install
-printf "#!/bin/bash" >> /usr/local/bin/vault
+printf "#!/bin/bash\n" >> /usr/local/bin/vault
 printf "npm --prefix /home/server start" >> /usr/local/bin/vault
 chmod +x /usr/local/bin/vault
 echo "* npm installed."
