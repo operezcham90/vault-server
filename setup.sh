52fd6e5ca52cb14f2c2d5c03616421c98fedb94a
operezcham <operezcham@gmail.com>
2021-11-29 22:09:10 -0600

    📘

--- setup.sh
+++ setup.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# install basic software
+# base packages
 apt -y update
 apt -y install git
 apt -y install npm
 apt -y install postgresql
 
-# set main folder
+# main folder
 rm -R /home/serv
 mkdir /home/serv
 cd /home/serv
 
-# get source code
+# source code
 git clone https://github.com/operezcham90/vault-server.git .
 npm install
 
-# get random secret
+# environment
 printf "APP_KEY=" >> .env
 uuidgen -r >> .env
 echo "HOST=0.0.0.0" >> .env
 echo "PORT=80" >> .env
 
-# set local database
+# database
 sudo -u postgres createdb vault
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'pass';"
 node ace.js migration:run
 
-# set daemon process
+# daemon
 npm install pm2 -g
 pm2 start server.js
 pm2 restart server.js
