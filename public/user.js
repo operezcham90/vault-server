f161f29c0daa6f792a30642c45d80b9633851069
operezcham <operezcham@gmail.com>
2021-12-02 00:38:11 -0600

    🌐

--- public/user.js
+++ public/user.js
@@ -1,1 +1,30 @@
 'use strict'
+
+const dom = {
+    name: (name) => {
+        return document.getElementsByName(name)
+    },
+    val: {
+        name: (name, val) => {
+            const list = dom.name(name)
+            for (const item of list) {
+                item.value = val;
+            }
+        }
+    }
+}
+
+const api = {
+    get: async (url) => {
+        const res = await fetch(url)
+        if (res.ok) {
+            return await res.json()
+        } else {
+            return {}
+        }
+    },
+    csrf: async () => {
+        const res = await api.get('/csrf')
+        dom.val.name('_csrf', res.token)
+    }
+}
