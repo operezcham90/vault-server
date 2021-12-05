72c3f582eb1c6fcafe2d04379dcb3de51f7deccc
operezcham <operezcham@gmail.com>
2021-12-05 14:10:23 -0600

    🌐

--- public/start.js
+++ public/start.js
@@ -1,7 +1,20 @@
 'use strict'
 
-const start = () => {
-    const div = document.createElement("div")
-    div.append("Hi ✋")
-    document.body.append(div)
+const view = {
+    name: 'hi',
+    root: document.createElement('div'),
+    show: (name = 'hi') => {
+        document.body.removeChild(view.root);
+        view.root = document.createElement('div')
+        view.write[name]()
+        document.body.append(view.root)
+    },
+    write: {
+        hi: () => {
+            const login = document.createElement('button')
+            login.value = 'Login 🔒'
+            login.onclick = view.show('login')
+            view.root.append(login)
+        }
+    }
 }
