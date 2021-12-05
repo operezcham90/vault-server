6d63b5dc5f1cab4d118cad9cf52bd0756873e300
operezcham <operezcham@gmail.com>
2021-12-05 14:15:34 -0600

    🌐

--- public/start.js
+++ public/start.js
@@ -4,7 +4,7 @@ const view = {
     name: 'hi',
     root: document.createElement('div'),
     show: (name = 'hi') => {
-        document.body.removeChild(view.root);
+        view.root.remove()
         view.root = document.createElement('div')
         view.write[name]()
         document.body.append(view.root)
