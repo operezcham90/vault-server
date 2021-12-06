d78885857fb98a9899a15524a9df4bdf10a855b3
operezcham <operezcham@gmail.com>
2021-12-05 20:57:12 -0600

    🌐

--- public/start.js
+++ public/start.js
@@ -1,2 +1,8 @@
 'use strict'
 
+document.onload = async () => {
+    const res = await fetch('/csrf')
+    const csrf = await response.json()
+    const inputs = document.getElementsByName('_csrf')
+    inputs.values = csrf.token
+}
