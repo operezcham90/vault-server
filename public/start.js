56b47502e276fb946bcd55a6c6c937c921b1ce2a
operezcham <operezcham@gmail.com>
2021-12-08 19:52:41 -0600

    🌐

--- public/start.js
+++ public/start.js
@@ -4,8 +4,13 @@ document.body.onload = async () => {
     const state = await fetch('/state')
     const { token, email } = await state.json()
     const inputs = document.getElementsByName('_csrf')
-    for (let input of inputs) {
+    for (const input of inputs) {
         input.value = token
     }
     document.getElementById('user').value = email
+    if (!document.getElementById('user').value) {
+        document.getElementById('login').classList.remove('invisible')
+    } else {
+        document.getElementById('logout').classList.remove('invisible')
+    }
 }
