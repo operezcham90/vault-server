72f688c456d836eeb7673952bd0c558d954b68d5
operezcham <operezcham@gmail.com>
2021-12-09 14:44:28 -0600

    🌐

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 'use strict'
 
-const { Ignitor, hooks } = require('@adonisjs/ignitor')
+const { Ignitor } = require('@adonisjs/ignitor')
 const https = require('https')
 const fs = require('fs')
 
-hooks.before.httpServer(() => {
-  var options = {
-    key: fs.readFileSync('./localhost.key'),
-    cert: fs.readFileSync('./localhost.crt'),
-  }
-  return https.createServer(options)
-})
-
-new Ignitor(require('@adonisjs/fold'))
+/*new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
   .fireHttpServer()
-  .catch(console.error)
+  .catch(console.error)*/
+
+new Ignitor(__dirname).httpServer().start((handle) => {
+  return https.createServer({
+    key: fs.readFileSync('./localhost.key'),
+    cert: fs.readFileSync('./localhost.crt'),
+  },
+    handle
+  );
+});
