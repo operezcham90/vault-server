7d66e16fbc37658da2af9ba48206c0e7526a5ff9
operezcham <operezcham@gmail.com>
2022-01-04 20:25:32 -0600

    💿

--- app/Controllers/Http/MainController.js
+++ app/Controllers/Http/MainController.js
@@ -85,15 +85,26 @@ class MainController {
                 uploads.push(request.files().uploads)
             }
             for (let i = 0; i < uploads.length; i++) {
+                let words = []
+                if (tags[i]) {
+                    words = tags[i].toLowerCase().replace(/[^\w\d ]/g, ' ').split(' ')
+                    const filter = []
+                    for (let i = 0; i < words.length; i++) {
+                        if (words.indexOf(words[i]) === i && words[i] !== '') {
+                            filter.push(words[i])
+                        }
+                    }
+                    words = filter.sort()
+                }
                 const id = uuid.v4()
                 const sum = crypto.createHash('sha256')
-                const buff = fs.readFileSync(uploads[i].tmpPath)
+                const buff = await fs.promises.readFile(uploads[i].tmpPath)
                 sum.update(buff)
                 const hex = sum.digest('hex')
                 const base = '/home/serv/uploads/' + id
                 await fs.promises.mkdir(base)
                 await fs.promises.writeFile(base + '/d.txt', hex)
-                await fs.promises.writeFile(base + '/t.txt', tags[i])
+                await fs.promises.writeFile(base + '/t.txt', words.join(' '))
                 await fs.promises.rename(uploads[i].tmpPath, base + '/f.' + uploads[i].extname)
             }
         } else {
@@ -103,11 +114,32 @@ class MainController {
     }
     async find({ auth, request, response }) {
         let { tags } = request.all()
-        if (auth.user && tags) {
-            tags = tags.split(' ')
+        if (auth.user) {
+            let words = []
+            if (tags) {
+                words = tags.toLowerCase().replace(/[^\w\d ]/g, ' ').split(' ')
+                const filter = []
+                for (let i = 0; i < words.length; i++) {
+                    if (words.indexOf(words[i]) === i && words[i] !== '') {
+                        filter.push(words[i])
+                    }
+                }
+                words = filter.sort()
+            }
             const path = '/home/serv/uploads'
             const ids = await fs.promises.readdir(path)
-            return ids
+            const files = []
+            for (const id in ids) {
+                const t = await fs.promises.readFile(path + '/' + id + '/t.txt', 'utf8')
+                const d = await fs.promises.readFile(path + '/' + id + '/d.txt', 'utf8')
+                const file = {
+                    id: id,
+                    hash: d,
+                    tags: t
+                }
+                files.push(file)
+            }
+            return files
         } else {
             response.status(401)
         }
