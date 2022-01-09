e534733b7dbd113898e2d350734933c7fa17b3a7
operezcham <operezcham@gmail.com>
2022-01-09 11:40:02 -0600

    💿

--- app/Models/Tags.js
+++ app/Models/Tags.js
@@ -2,15 +2,14 @@
 
 class Tags {
     static parse(tags) {
-        let words = tags.toLowerCase()
-        words = words.trim()
-        words = words.replace(/[^\w\d_ ]/g, ' ')
-        words = words.split(' ')
-        words = [...new Set(words)]
-        if (words[0] === '') {
-            words.shift()
-        }
-        return words.sort()
+        const alphanum = tags.replace(/[^\w\d ]/g, ' ')
+        const lower = alphanum.toLowerCase()
+        const cut = lower.trim()
+        const words = cut.split(/\s+/g)
+        const order = words.sort()
+        const unique = new Set(order)
+        const array = Array.from(unique)
+        return array
     }
 }
 
